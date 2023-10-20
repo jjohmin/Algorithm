51ffd30dafe55bc4f80f255dc3767e4c587c0cac
jjohmin <128464783+jjohmin@users.noreply.github.com>
2023-10-20 19:18:31 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 0.17 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+// queries_rows는 2차원 배열 queries의 행 길이, queries_cols는 2차원 배열 queries의 열 길이입니다.
+int* solution(int arr[], size_t arr_len, int** queries, size_t queries_rows, size_t queries_cols) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(100001);
+    int s,e,k;
+    int min, cnt=0;
+    for(int i = 0; i < queries_rows; i++){
+        min = 1000001;
+        s = queries[i][0];
+        e = queries[i][1];
+        k = queries[i][2];
+        for(int j = s;j<=e;j++)
+            if(arr[j]>k)
+                if(min>arr[j])
+                    min = arr[j];
+        if(min==1000001)
+            min=-1;
+        answer[cnt++] = min;
+    }
+    return answer;
+}
