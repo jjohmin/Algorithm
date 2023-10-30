0787b674b4c08f97dd6792f68526858a47b3ce38
jjohmin <128464783+jjohmin@users.noreply.github.com>
2023-10-30 18:37:54 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	char arr[5][15] = { 0 };
+	for (int i = 0; i < 5; i++) {
+		scanf("%s", arr[i],sizeof(arr));
+	}
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (arr[j][i] == NULL)
+				continue;
+			printf("%c", arr[j][i]);
+		}
+	}
+	return 0;
+}
