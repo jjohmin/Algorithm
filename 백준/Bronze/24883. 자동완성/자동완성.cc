1d44e4183d52c25becf087893e8a18aec414f85f
jjohmin <128464783+jjohmin@users.noreply.github.com>
2023-12-29 23:52:50 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char n;
+	cin >> n;
+	if (n == 'N' || n == 'n')
+		cout << "Naver D2" << endl;
+	else
+		cout << "Naver Whale" << endl;
+}
