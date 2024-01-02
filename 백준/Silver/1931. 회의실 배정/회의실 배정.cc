6ad14f97d1de59a1665f0e9fc1f78578ceda50d6
jjohmin <128464783+jjohmin@users.noreply.github.com>
2024-01-02 15:19:04 +0900

    [Silver I] Title: 회의실 배정, Time: 84 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<pair<int, int>> A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i].second;
+		cin >> A[i].first;
+	}
+	sort(A.begin(), A.end());
+	int count = 0;
+	int end = -1;
+	for (int i = 0; i < N; i++) {
+		if (A[i].second >= end) {
+			end = A[i].first;
+			count++;
+		}
+	}
+	cout << count << endl;
+}
