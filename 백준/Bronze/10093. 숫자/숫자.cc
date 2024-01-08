5a41bb54a9fa39ed0dd870e68593b5b69cf66899
jjohmin <128464783+jjohmin@users.noreply.github.com>
2024-01-08 18:52:09 +0900

    [Bronze II] Title: 숫자, Time: 72 ms, Memory: 80152 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -4,20 +4,18 @@
 using namespace std;
 
 int main() {
-	long long int n1, n2,cnt=0;
-	vector<long long int> A(10000000);
+	unsigned long long n1, n2,cnt=0;
+	vector<unsigned long long> A(10000000);
 	cin >> n1 >> n2;
 	if (n1 > n2) {
-		int temp = n1;
-		n1 = n2;
-		n2 = temp;
+		swap(n1, n2);
 	}
-	for (long long int i = n1 + 1; i < n2; i++) {
+	for (unsigned long long i = n1 + 1; i < n2; i++) {
 		A[cnt++] = i;
 	}
 	A.resize(cnt);
 	sort(A.begin(), A.end());
 	cout << cnt << "\n";
-	for (int i = 0; i < cnt; i++)
+	for (unsigned long long i = 0; i < cnt; i++)
 		cout << A[i] << " ";
 }
