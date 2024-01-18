52de9173c283f3dea2b3f7994cb948b756628c92
jjohmin <128464783+jjohmin@users.noreply.github.com>
2024-01-18 14:40:04 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 124 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N,num;
+	cin >> N;
+	vector<bool> A(1000001);
+	vector<bool> v(1000001);
+	fill(A.begin(), A.end(), true);
+	A[0] = A[1] = false;
+	for (long long int i = 2; i <= sqrt(1000000); i++) {
+		if (A[i]) {
+			long long int j = 2;
+			while (i * j <= 1000000) {
+				A[i * j] = false;
+				j += 1;
+			}
+		}
+	}
+	for (long long int i = 0; i < N; i++) {
+		long long int cnt = 0;
+		fill(v.begin(), v.end(), true);
+		cin >> num;
+		for (long long int j = 2; j < num; j++) {
+			if (A[j] && A[num - j] && v[j]) {
+				cnt++;
+				v[j] = false;
+				v[num - j] = false;
+			}
+		}
+		cout << cnt << "\n";
+	}
+}
