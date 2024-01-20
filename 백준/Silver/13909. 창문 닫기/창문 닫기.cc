6794fa0442fa81b1a03e4318c3a777170f2d3caf
jjohmin <128464783+jjohmin@users.noreply.github.com>
2024-01-20 11:38:24 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long int N;
+	cin >> N;
+	cout << (int)sqrt(N);
+}
