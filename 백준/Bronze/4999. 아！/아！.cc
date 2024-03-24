79da8d5e756c0b416f8f17c894c1d3f7a0dced24
jjohmin <128464783+jjohmin@users.noreply.github.com>
2024-03-25 08:14:23 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	if (a.length() >= b.length())
+		cout << "go";
+	else
+		cout << "no";
+}
